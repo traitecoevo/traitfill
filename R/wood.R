8d9d5097b660faa37c0fd0ad4bf3a6a86e9dc8d5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-04-27 16:51:24 +1000

    Export `load_wood` for use in examples

--- R/wood.R
+++ R/wood.R
@@ -1,3 +1,8 @@
+##' Data from the woodiness paper
+##' @title Data from the woodiness paper
+##' @export
+##' @examples
+##' head(load_wood())
 load_wood <- function() {
   read.csv(system.file("extdata/wood.csv", package=.packageName,
                        mustWork=TRUE),
